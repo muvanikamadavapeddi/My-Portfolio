611621e1ac3c627f12ad4fb32c62f56067397c83
muvanikamadavapeddi <149654726+muvanikamadavapeddi@users.noreply.github.com>
2024-05-23 05:46:13 +0000

    set homepages for github pages deploy

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,9 +4,6 @@ const Education = () => {
     return(
         <section className="light" id="education">
             <h2>Education</h2>
-            <h3>Bachelor of Technology</h3>
-            <p>CMR Institute of Technology</p>
-            <p>2022-2026</p>
         </section>
     )
 }
